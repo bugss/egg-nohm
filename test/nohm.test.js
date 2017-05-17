9bbb0a93f733f6d2c7c74213c0e94ddffcb48505
huangxuan2046 <312120511@qq.com>
2017-05-17 22:21:14 +0800

    first commit

--- test/nohm.test.js
+++ test/nohm.test.js
@@ -65,7 +65,6 @@ describe('test/nohm.test.js', () => {
     mm.app({
       baseDir: 'apps/nohm-config-redis-return-null',
     }).on('error', function(err) {
-      console.info(err);
       done();
     });
 
